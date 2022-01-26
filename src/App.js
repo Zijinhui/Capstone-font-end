69e44d7da0c39eab05a52094786b41894cec26fb
Zixin Zou <zxinmm@gmail.com>
2022-01-25 19:02:30 -0500

    index => App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Signup from './components/Auth/Signup';
+import Menu from './components/Menu/Menu';
+import Cart from './components/Cart/Cart'
+import Food from './components/Menu/Food';
+import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
 
 function App () {
     return(
-        <>hi</>
+        <Router>
+            <Routes>
+                <Route path='/' element={<Nav />}>
+                    <Route path='/home' element={<Home />}/>
+                    <Route path='/menu' element={<Menu />} />
+                    <Route path='/menu/:type' element={<Food />} />
+                    <Route path='/login' element={<Home />}/>
+                    <Route path='/signup' element={<Signup />}/>
+                    <Route path='cart' element={<Cart />}/>
+                    <Route path='payment' element={<Home />}/>
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,28 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import reportWebVitals from './reportWebVitals';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import Signup from './components/auth/Signup';
-import Menu from './components/Menu/Menu';
-import Cart from './components/Cart/Cart'
-import Food from './components/Menu/Food';
-import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
+import App from './App';
+
 
 ReactDOM.render(
-  <Router>
-      <Routes>
-         <Route path='/' element={<Nav />}>
-            <Route path='/home' element={<Home />}/>
-            <Route path='/menu' element={<Menu />} />
-            <Route path='/menu/:type' element={<Food />} />
-            <Route path='/login' element={<Home />}/>
-            <Route path='/signup' element={<Signup />}/>
-            <Route path='cart' element={<Cart />}/>
-            <Route path='payment' element={<Home />}/>
-          </Route>
-      </Routes>
-  </Router>,
+  <App />,
   document.getElementById('root')
 );
 
