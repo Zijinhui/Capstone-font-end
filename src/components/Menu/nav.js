4d4f5de0aba84b0ac5eda3dd02a031ac99b85f87
Zijinhui <79233742+Zijinhui@users.noreply.github.com>
2022-01-27 11:11:35 -0500

    unfinished menu layout

--- src/components/Menu/nav.js
+++ src/components/Menu/nav.js
@@ -5,17 +5,19 @@ export default function Nav(){
 
     return(
         <>
-             <nav>
-                <Link to="/menu">All</Link>
-                <Link to="/menu/salad">Salad</Link>
-                <Link to="/menu/appetizer">Appetizer</Link>
-                <Link to="/menu/soup">Soup</Link>
-                <Link to="/menu/rolls">Rolls</Link>
-                <Link to="/menu/bento">Bento</Link>
-                <Link to="/menu/rice">Rice</Link>
-                <Link to="/menu/combo">Combo</Link>
-                <Link to="/menu/sushi">Sushi/Sashimi</Link> 
+             <nav className="side-bar">
+                 <ul className="categories">
+                <li><Link to="/menu">All</Link></li>
+                <li><Link to="/menu/salad">Salad</Link></li>
+                <li> <Link to="/menu/appetizer">Appetizer</Link></li>
+                <li> <Link to="/menu/soup">Soup</Link></li>
+                <li><Link to="/menu/rolls">Rolls</Link></li>
+                <li><Link to="/menu/bento">Bento</Link></li>
+                <li><Link to="/menu/rice">Rice</Link></li>
+                <li><Link to="/menu/combo">Combo</Link></li>
+                <li><Link to="/menu/sushi">Sushi/Sashimi</Link></li>
                 {/* Sushi&Sashimi */}
+                </ul>
             </nav>
 
             <Outlet/>
