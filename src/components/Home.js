065a14be52a5b5ff51451c7a448d7c325fb84e4b
Zijinhui <79233742+Zijinhui@users.noreply.github.com>
2022-01-26 22:51:16 -0500

    Add gif

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,8 @@ function Home() {
           <p>[Sushi restaurant]</p>
           <p>We are open on Mondays, from 4-6 pm</p>
           <p>All are welcome, except for those that don't like sushi</p>
+          <img src={require('../img/sleeping.gif')} className="sleep-sushi"/>
+          <h2>About Us</h2>
         </div>
       );
 
