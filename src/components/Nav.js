c51077fc154fe43aafe552415a614ccc346b8bcd
Zijinhui <79233742+Zijinhui@users.noreply.github.com>
2022-01-27 03:36:15 -0500

    Add dropdown menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Link,Outlet} from 'react-router-dom';
-import { Container, FormControl, Navbar, Nav, Dropdown, Badge } from 'react-bootstrap'
+import { Container, FormControl, Navbar, Nav, Dropdown, Badge , Offcanvas, NavDropdown, Form, Button} from 'react-bootstrap'
 import { IoCartOutline } from 'react-icons/io5'
 import { GiSushis } from 'react-icons/gi'
+import { TiThMenuOutline } from 'react-icons/ti'
 import { useLayoutEffect, useState } from 'react';
-
-
 
 export default function NavBar(){
     function useWindowSize() {
@@ -30,21 +29,26 @@ export default function NavBar(){
         }
         return smallScreen
       }
-      
-      
-
+    
     return(
         <>
-            <Navbar bg="dark" variant="dark"  style={{ height: 80}}>
-                <Container>
+            <Navbar bg="dark" variant="dark" expand={'lg'} style={{ height: 80}}>
+                <Container >
                    
                     <nav className='nav'>
                         <GiSushis color="white" fontSize="33px"/>
                         <h3 className='sushi-heading'>すし</h3>
 
                         { ShowWindowDimensions() ?
-                        <></>
-                        :
+                        <NavDropdown title={ <TiThMenuOutline color="white" fontSize="25px"/>}id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="/home">Home</NavDropdown.Item>
+                            <NavDropdown.Item href="/menu">Menu</NavDropdown.Item>
+                            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                            <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/payment">Payment</NavDropdown.Item>
+                            </NavDropdown>
+                        :                  
                         <>
                             <Link className='nav-link' to="/home">Home</Link>
                             <Link className='nav-link' to="/menu">Menu</Link>
@@ -53,14 +57,14 @@ export default function NavBar(){
                             <Link className='nav-link' to="/cart">Cart</Link>
                             <Link className="nav-link" to="/payment">Payment</Link>
                         </>   
-                        }
+                        }      
                     </nav>  
                     
                 </Container>
 
                 {/* delete Signup after login page created */}
                 <Nav>
-                    <Dropdown alignRight>
+                    <Dropdown >
                         <Dropdown.Toggle variant="success">
                         <IoCartOutline color="white" fontSize="25px"/>
                         <Badge>{10}</Badge>
