baa01c8492ad1e57832ea2e993ccb3a124fc818c
Oleksii Kravtsov <bahbok1@gmail.com>
2022-01-27 10:19:08 -0500

    fixed more styling in the cart page

--- src/components/Cart/Order.js
+++ src/components/Cart/Order.js
@@ -4,14 +4,16 @@ function Order(props){
     console.log(props)
     return (
         <div className='order flex flex-el'>
-            <img alt='[image]'/>
-            {props.id && <div>
-                <div>{props.name}</div>                        
+            <img className='sushi-icon' alt='[image]'/>
+            {props.id && <div className='order-details'>
+                <div className='order-name'>{props.name}</div>                        
                 <div className='flex'>
                     <div className='price'>${props.price}</div>
-                    <button onClick={()=>props.onClick(props.id,-1)}>-</button>
-                    <div className='price'>{props.qty}</div>
-                    <button onClick={()=>props.onClick(props.id,1)}>+</button>
+                    <div className='flex price-change'>
+                        <button onClick={()=>props.onClick(props.id,-1)}>-</button>
+                        <div className='price'>{props.qty}</div>
+                        <button onClick={()=>props.onClick(props.id,1)}>+</button>
+                    </div>
                 </div>
             </div>}
         </div>
