c1614677a7b538c0448aa0b1fa8e18c274ece49f
kychennn <kallychen2@gmail.com>
2022-01-27 22:30:24 -0500

    update cart page css

--- src/components/Cart/Order.js
+++ src/components/Cart/Order.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { CartState } from '../GlobalContext';
+import { Button } from 'react-bootstrap'
 
 function Order(props){
-
-    console.log(props.food.food)
 
     const {dispatch} = CartState()
 
@@ -15,19 +14,25 @@ function Order(props){
     }
 
     return (
-        <div className='order flex flex-el'>
-            <img className='sushi-icon' alt='[image]'/>
-            {props.food.food.id && <div>
-                <div className='order-name'>{props.food.food.name}</div>                        
-                <div className='flex'>
-                    <div className='price price-change'>${props.food.food.price}</div>
-                    <button onClick={()=>handleChange(props.food.food,'DECREMENT_IN_CART')}>-</button>
-                    <div className='price'>{props.food.qty}</div>
-                    <button onClick={()=>handleChange(props.food.food,'INCREMENT_IN_CART')}>+</button>
+        <tbody>
+            <tr>
+                <th scope="row" class="border-0">
+                <div class="p-2">
+                    <img src={props.image} alt='[image]' width="350" class="img-fluid rounded shadow-sm" />
+                     <div class="ms-3 d-inline-block align-middle">
+                    <h5 class="mb-0"><a href="#" class="text-dark d-inline-block align-middle">{props.food.food.name}</a></h5>
+                    </div>
+                    </div>
+                </th> 
 
-                </div>
-            </div>}
-        </div>
+                <td class="border-0 align-middle"><strong>${props.food.food.price}</strong></td>
+                
+                <Button className='cartBtn' onClick={()=>handleChange(props.food.food,'DECREMENT_IN_CART')}>-</Button>
+                <td class="border-0 align-middle"><strong>{props.food.qty}</strong></td>
+                <Button className='cartBtn2' onClick={()=>handleChange(props.food.food,'INCREMENT_IN_CART')}>+</Button>
+            </tr>
+        </tbody>
+        
     )
 }
 
