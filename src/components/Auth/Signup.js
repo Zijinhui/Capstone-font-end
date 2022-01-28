6978abf15dd781058608d0d69e00ea0cfea1b2e8
Zixin Zou <zxinmm@gmail.com>
2022-01-28 02:26:02 -0500

    axois missing in Signup

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "./AuthContext"
 import { Link, useNavigate } from "react-router-dom"
+import axios from 'axios'
 
 export default function Signup() {
   const emailRef = useRef()
